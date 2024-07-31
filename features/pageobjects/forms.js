0330254bf8f02edc54aa31577ca0400e6352b804
ajoejoseph369 <ajoe.joseph@people10.com>
2024-07-31 15:18:53 +0530

    working

---
+++ features/pageobjects/forms.js
@@ -0,0 +1,78 @@
+const {$} = require('@wdio/globals');
+// const { ok } = require('appium-doctor/build/lib/utils');
+const page = require('./page');
+
+const formsBtn = '//XCUIElementTypeButton[@name="Forms"]';
+// const formsView = '//XCUIElementTypeOther[@name="Forms-screen"]';
+const pageHeader = '//XCUIElementTypeStaticText[@name="Form components"]';
+const textInput = '//XCUIElementTypeTextField[@name="text-input"]';
+const enteredText = '//XCUIElementTypeStaticText[@name="input-text-result"]';
+const toggleBtn = '//XCUIElementTypeSwitch[@name="switch"]';
+const switchText = '//XCUIElementTypeStaticText[@name="switch-text"]';
+const dropdown = '//XCUIElementTypeOther[@name="Dropdown"]';
+const pickerWheel = '//XCUIElementTypePickerWheel';
+// const firstOption = '//XCUIElementTypePickerWheel[@value="webdriver.io is awesome"]'
+// const secondOption = '//XCUIElementTypePickerWheel[@value="Appium is awesome"]';
+const done = '//XCUIElementTypeOther[@name="done_button"]';
+const activeBtn = '//XCUIElementTypeOther[@name="Active"]';
+const btnPressConfirmation = '//XCUIElementTypeAlert[@name="This button is"]';
+const okBtn = '//XCUIElementTypeButton[@name="OK"]';
+
+class Forms{
+
+    async redirectToFormsPage(){
+        // while(!(await $(pageHeader).isDisplayed())){
+        await $(formsBtn).click();
+        await $(pageHeader).waitForDisplayed({timeout:6000});
+        // }
+    }
+
+    async checkRedirectionToFormsPage(){
+        // await $(formsView).waitForExist({timeout:6000});
+        if(await $(pageHeader).isDisplayed())
+            console.log("On Forms Page");
+        else
+            console.log("not on forms page yet");
+        return (await $(pageHeader).isDisplayed());
+    }
+
+    async enterText(text){
+        await $(textInput).setValue(text);
+    }
+
+    async checkEnteredText(text){
+        return (await $(enteredText)==text);
+    }
+
+    async clickToggleBtn(){
+        await $(toggleBtn).click();
+    }
+
+    async checkToggleBtnClick(){
+        return (await $(switchText)=="Click to turn the switch OFF");
+    }
+
+    async selectDropDown(){
+        await $(dropdown).click();
+        // await $(pickerWheel).waitForDisplayed({timeout:8000});
+        await $(pickerWheel).setValue('Appium is awesome');
+        // await $(firstOption).waitForDisplayed({ timeout: 5000 });
+        // await $(secondOption).waitForDisplayed({ timeout: 5000 });
+        await $(done).click();
+    }
+
+    async clickActiveBtn(){
+        await $(activeBtn).click();
+    }
+
+    async checkBtnClick(){
+        if(await $(btnPressConfirmation).isDisplayed()){
+            await $(okBtn).click();
+            return true;
+        }
+        await browser.pause(2000);
+    }
+
+}
+
+module.exports = new Forms();
---
+++ features/pageobjects/swipe.js
@@ -0,0 +1,69 @@
+const {$,driver} = require('@wdio/globals');
+
+const swipe = '//XCUIElementTypeButton[@name="Swipe"]';
+const pageHeader = '//XCUIElementTypeStaticText[@name="Swipe horizontal"]'
+const carousel = '//XCUIElementTypeOther[@name="Carousel"]';
+
+
+class Swipe{
+
+    async goToSwipePage(){
+        while(!(await $(pageHeader).isDisplayed())){
+            await $(swipe).click();
+        }
+        await $(pageHeader).waitForExist({timeout:6000});
+        return(await $(pageHeader).isDisplayed());
+
+    }
+
+    async swipeLeft(){
+        const size = await $(carousel).getSize();
+        const location = await $(carousel).getLocation();
+
+        const startX = location.x + size.width * 0.9; // Start near the right edge
+        const startY = location.y + size.height / 2; // Start vertically centered
+        const endX = location.x + size.width * 0.1; // End near the left edge
+        const endY = startY; // End vertically centered
+
+        
+        await browser.performActions([{
+            type: 'pointer',
+            id: 'finger1',
+            parameters: { pointerType: 'touch' },
+            actions: [
+                { type: 'pointerMove', duration: 0, x: startX, y: startY },
+                { type: 'pointerDown', button: 0 },
+                { type: 'pause', duration: 500 },
+                { type: 'pointerMove', duration: 500, origin: 'pointer', x: endX - startX, y: endY - startY },
+                { type: 'pointerUp', button: 0 }
+            ]
+        }]);
+    
+        await browser.releaseActions();
+    }
+
+    async swipeToEndOfCarousel() {
+        let reachedEnd = false;
+        let maxSwipes = 6; // carousel length
+        while (!reachedEnd && maxSwipes > 0) {
+            try {
+                await this.swipeLeft();
+    
+                const lastItem = await $('//XCUIElementTypeOther[@name="card" and @label="尿 COMPATIBLE WebdriverIO works in combination with most of the TDD and BDD test frameworks in the JavaScript world"]');
+                if (await lastItem.isDisplayed()) {
+                    reachedEnd = true;
+                }
+            } catch (error) {
+                console.error('Error during swipe:', error);
+            }
+    
+            maxSwipes--;
+        }
+
+        if (!reachedEnd) {
+            console.log('Reached the maximum number of swipes without finding the end of the carousel.');
+        }
+    }
+}
+
+module.exports = new Swipe();
